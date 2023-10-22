a5f48d6f31224f5e2278a74c77bfae97ad6f6f38
dimensionnn <t.golovin55@gmail.com>
2023-10-22 13:41:05 +0600

    fix/fixed text-icon-left button styles

--- src/components/UI/button/text-icon-left/styles.ts
+++ src/components/UI/button/text-icon-left/styles.ts
@@ -4,31 +4,18 @@ import {ColorThemes} from 'src/hooks/useColorTheme';
 import {StyleSheet} from 'react-native';
 
 export const textIconLeftButtonStyles = {
-  dark: {
-    text: StyleSheet.create({
-      pressed: {color: COLORS.secondary_dark_01},
-      disabled: {color: COLORS.grayscale_dark_04},
-      initial: {color: COLORS.grayscale_dark_01},
-    }),
-    icon: {
-      pressed: {fill: COLORS.secondary_dark_01},
-      disabled: {fill: COLORS.grayscale_dark_04},
-      initial: {fill: COLORS.grayscale_dark_01},
-    },
+  [ColorThemes.DARK]: {
+    pressed: {color: COLORS.secondary_dark_01},
+    disabled: {color: COLORS.grayscale_dark_04},
+    initial: {color: COLORS.grayscale_dark_01},
   },
-  light: {
-    text: StyleSheet.create({
-      pressed: {color: COLORS.secondary_light_01},
-      disabled: {color: COLORS.grayscale_light_05},
-      initial: {color: COLORS.grayscale_light_01},
-    }),
-    icon: {
-      pressed: {fill: COLORS.secondary_light_01},
-      disabled: {fill: COLORS.grayscale_light_05},
-      initial: {fill: COLORS.grayscale_light_01},
-    },
+  [ColorThemes.LIGHT]: {
+    pressed: {color: COLORS.secondary_light_01},
+    disabled: {color: COLORS.grayscale_light_05},
+    initial: {color: COLORS.grayscale_light_01},
   },
   root: StyleSheet.create({
+    fontText: outfitTextStyles.bodyRegular_18,
     container: {
       width: '100%',
       maxWidth: 131,
@@ -36,64 +23,21 @@ export const textIconLeftButtonStyles = {
       flexDirection: 'row',
       justifyContent: 'center',
     },
-    font: outfitTextStyles.bodyRegular_18,
   }),
 };
 
-export const getButtonStyleSheet = (
+export const getButtonStyles = (
   themeVariant: ColorThemes,
-  pressed: boolean,
+  pessed?: boolean,
   isDisabled?: boolean,
 ) => {
-  switch (themeVariant) {
-    default:
-    case ColorThemes.DARK:
-      return {
-        container: [textIconLeftButtonStyles.root.container],
-        font: textIconLeftButtonStyles.root.font,
-        text: [
-          textIconLeftButtonStyles.dark.text.initial,
-          pressed && textIconLeftButtonStyles.dark.text.pressed,
-          isDisabled && textIconLeftButtonStyles.dark.text.disabled,
-        ],
-      };
-    case ColorThemes.LIGHT:
-      return {
-        container: [textIconLeftButtonStyles.root.container],
-        font: textIconLeftButtonStyles.root.font,
-        text: [
-          textIconLeftButtonStyles.light.text.initial,
-          pressed && textIconLeftButtonStyles.light.text.pressed,
-          isDisabled && textIconLeftButtonStyles.light.text.disabled,
-        ],
-      };
-  }
-};
-
-export const getIconColor = (
-  themeVariant: ColorThemes,
-  pressed: boolean,
-  isDisabled?: boolean,
-) => {
-  switch (themeVariant) {
-    default:
-    case ColorThemes.DARK: {
-      if (pressed) {
-        return textIconLeftButtonStyles.dark.icon.pressed;
-      }
-      if (isDisabled) {
-        return textIconLeftButtonStyles.dark.icon.disabled;
-      }
-      return textIconLeftButtonStyles.dark.icon.initial;
-    }
-    case ColorThemes.LIGHT: {
-      if (pressed) {
-        return textIconLeftButtonStyles.light.icon.pressed;
-      }
-      if (isDisabled) {
-        return textIconLeftButtonStyles.light.icon.disabled;
-      }
-      return textIconLeftButtonStyles.light.icon.initial;
-    }
-  }
+  return {
+    container: textIconLeftButtonStyles.root.container,
+    font: textIconLeftButtonStyles.root.fontText,
+    text: [
+      textIconLeftButtonStyles[themeVariant].initial,
+      pessed && textIconLeftButtonStyles[themeVariant].pressed,
+      isDisabled && textIconLeftButtonStyles[themeVariant].disabled,
+    ],
+  };
 };
--- src/components/UI/button/text-icon-left/text-icon-left.tsx
+++ src/components/UI/button/text-icon-left/text-icon-left.tsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import {ColorThemes, useColorTheme} from 'src/hooks/useColorTheme';
 import {Pressable, Text} from 'react-native';
-import {getButtonStyleSheet, getIconColor} from './styles';
+import {getButtonStyles} from './styles';
 
 interface ButtonProps {
   title?: string;
@@ -13,8 +13,7 @@ export const Button: React.FC<ButtonProps> = ({title, isDisabled, onPress}) => {
   const [isPressed, setIsPressed] = useState(false);
   const themeVariant: ColorThemes = useColorTheme();
 
-  const buttonStyles = getButtonStyleSheet(themeVariant, isPressed, isDisabled);
-  //const iconStyles = getIconColor(themeVariant, isPressed, isDisabled);
+  const buttonStyles = getButtonStyles(themeVariant, isPressed, isDisabled);
 
   const handlePress = () => {
     setIsPressed(!isPressed);
