8e57013963fabfbe1de8a8766535c0328020504f
dimensionnn <t.golovin55@gmail.com>
2023-10-23 13:18:48 +0600

    fix/fixed icons

--- src/shared/icons/components/user-mini.tsx
+++ src/shared/icons/components/user-mini.tsx
@@ -1,11 +1,10 @@
 import * as React from 'react';
-import Svg, {SvgProps, G, Path, Defs, ClipPath} from 'react-native-svg';
+import Svg, {SvgProps, G, Path} from 'react-native-svg';
 export const SvgUserMini = (props: SvgProps) => (
   <Svg
-    xmlns="http://www.w3.org/2000/svg"
     width={24}
     height={24}
-    fill="none"
+    fill="currentColor"
     viewBox="0 0 24 24"
     {...props}>
     <G clipPath="url(#a)">
@@ -21,10 +20,5 @@ export const SvgUserMini = (props: SvgProps) => (
         d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
       />
     </G>
-    <Defs>
-      <ClipPath id="a">
-        <Path fill="currentColor" d="M0 0h24v24H0z" />
-      </ClipPath>
-    </Defs>
   </Svg>
 );
