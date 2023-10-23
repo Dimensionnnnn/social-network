98ff841cdd0a15b2a93b65dad06b81890fcf1dab
dimensionnn <t.golovin55@gmail.com>
2023-10-23 13:52:51 +0600

    fix/fixed def btn, fix icon btn color, del logs

--- src/components/UI/button/default-button/default-button-styles.ts
+++ src/components/UI/button/default-button/default-button-styles.ts
@@ -2,9 +2,10 @@ import {outfitTextStyles} from 'src/assets/styles/typography';
 import {COLORS} from 'src/assets/styles/colors';
 import {ButtonSize} from './default-button';
 import {ColorThemes} from 'src/hooks/useColorTheme';
+import {StyleSheet} from 'react-native';
 
 export const defaultButtonStyles = {
-  dark: {
+  [ColorThemes.DARK]: {
     button: {
       pressed: {
         small: {backgroundColor: COLORS.secondary_dark_01},
@@ -13,22 +14,12 @@ export const defaultButtonStyles = {
       },
       disabled: {},
       delete: {},
-      small: {
-        backgroundColor: COLORS.grayscale_dark_07,
-      },
-      medium: {
-        backgroundColor: COLORS.grayscale_dark_07,
-      },
-      large: {
-        backgroundColor: COLORS.grayscale_dark_05,
-      },
+      small: {backgroundColor: COLORS.grayscale_dark_07},
+      medium: {backgroundColor: COLORS.grayscale_dark_07},
+      large: {backgroundColor: COLORS.grayscale_dark_05},
     },
     text: {
-      pressed: {
-        small: {color: COLORS.grayscale_dark_07},
-        medium: {color: COLORS.grayscale_dark_07},
-        large: {color: COLORS.grayscale_dark_07},
-      },
+      pressed: {color: COLORS.grayscale_dark_07},
       delete: {color: COLORS.additional_error},
       disabled: {color: COLORS.grayscale_dark_04},
       small: {color: COLORS.secondary_dark_01},
@@ -36,46 +27,27 @@ export const defaultButtonStyles = {
       large: {color: COLORS.secondary_dark_01},
     },
   },
-  light: {
+  [ColorThemes.LIGHT]: {
     button: {
       pressed: {
         small: {backgroundColor: COLORS.secondary_light_01},
         medium: {backgroundColor: COLORS.dark_mode},
         large: {backgroundColor: COLORS.secondary_light_01},
       },
-      disabled: {
-        backgroundColor: COLORS.grayscale_light_05,
-      },
-      delete: {
-        backgroundColor: COLORS.grayscale_light_07,
-      },
-      small: {
-        backgroundColor: COLORS.dark_mode,
-      },
-      medium: {
-        backgroundColor: COLORS.grayscale_light_07,
-      },
-      large: {
-        backgroundColor: COLORS.dark_mode,
-      },
+      disabled: {backgroundColor: COLORS.grayscale_light_05},
+      delete: {backgroundColor: COLORS.grayscale_light_07},
+      small: {backgroundColor: COLORS.dark_mode},
+      medium: {backgroundColor: COLORS.grayscale_light_07},
+      large: {backgroundColor: COLORS.dark_mode},
     },
     text: {
-      pressed: {
-        small: {color: COLORS.grayscale_light_07},
-        medium: {color: COLORS.grayscale_light_07},
-        large: {color: COLORS.grayscale_light_07},
-      },
+      pressed: {color: COLORS.grayscale_light_07},
       delete: {color: COLORS.additional_error},
       disabled: {color: COLORS.grayscale_light_03},
       small: {color: COLORS.grayscale_light_07},
       medium: {color: COLORS.dark_mode},
       large: {color: COLORS.grayscale_light_07},
     },
-  },
-  container: {
-    width: '100%',
-    maxWidth: 343,
-    alignItems: 'center',
   },
   sizes: {
     small: {
@@ -101,36 +73,48 @@ export const defaultButtonStyles = {
       borderRadius: 21,
     },
   },
-  text: outfitTextStyles.bodyMedium_16,
+  root: StyleSheet.create({
+    fontText: outfitTextStyles.bodyRegular_18,
+    container: {
+      width: '100%',
+      maxWidth: 343,
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'center',
+    },
+  }),
 };
 
-export enum ButtonElement {
-  button = 'button',
-  text = 'text',
+interface ButtonStylesOptions {
+  themeVariant: ColorThemes;
+  buttonSize: ButtonSize;
+  isPressed?: boolean;
+  isDisabled?: boolean;
+  isDelete?: boolean;
 }
 
-export const getButtonStyles = (
-  themeVariant: ColorThemes,
-  type: ButtonElement,
-  pressed: boolean,
-  buttonSize: ButtonSize,
-  isDelete?: boolean,
-  isDisabled?: boolean,
-) => {
-  const theme = defaultButtonStyles[themeVariant];
-
-  const baseStyles = theme[type][buttonSize];
-
-  const specificStyles = {
-    delete: isDelete && theme[type].delete,
-    disabled: isDisabled && theme[type].disabled,
-    pressed: pressed && theme[type].pressed[buttonSize],
+export const getButtonStyles = ({
+  themeVariant,
+  buttonSize,
+  isPressed,
+  isDisabled,
+  isDelete,
+}: ButtonStylesOptions) => {
+  return {
+    container: defaultButtonStyles.root.container,
+    fontText: defaultButtonStyles.root.fontText,
+    sizeContainer: defaultButtonStyles.sizes[buttonSize],
+    button: [
+      defaultButtonStyles[themeVariant].button[buttonSize],
+      isDelete && defaultButtonStyles[themeVariant].button.delete,
+      isPressed && defaultButtonStyles[themeVariant].button.pressed[buttonSize],
+      isDisabled && defaultButtonStyles[themeVariant].button.disabled,
+    ],
+    text: [
+      defaultButtonStyles[themeVariant].text[buttonSize],
+      isDelete && defaultButtonStyles[themeVariant].text.delete,
+      isPressed && defaultButtonStyles[themeVariant].text.pressed,
+      isDisabled && defaultButtonStyles[themeVariant].text.disabled,
+    ],
   };
-
-  return [
-    baseStyles,
-    specificStyles.delete,
-    specificStyles.disabled,
-    specificStyles.pressed,
-  ];
 };
