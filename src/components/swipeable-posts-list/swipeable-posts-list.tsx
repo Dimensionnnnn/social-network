38e469f616a1d94ab05e2b6dc09a1b2eada302a6
dimensionnn <t.golovin55@gmail.com>
2023-11-07 17:28:11 +0600

    feat/added my posts screen

---
+++ src/components/swipeable-posts-list/swipeable-posts-list.tsx
@@ -0,0 +1,84 @@
+import React from 'react';
+import {View, FlatList} from 'react-native';
+import {Spinner} from 'src/components/UI/spinner/spinner';
+import {PostCard} from 'src/components/UI/post-card/post-card';
+import {ListEmptyComponent} from 'src/components/UI/list-empty/list-empty';
+import {formatAuthorName} from 'src/helpers/formatAuthorName';
+import {useColorTheme} from 'src/hooks/useColorTheme';
+import {FavouritePosts} from 'src/api/posts/gql/querys/__generated__/get-favourite-posts.qury';
+import {Posts} from 'src/api/posts/gql/querys/__generated__/get-posts.query';
+import Swipeable from 'react-native-gesture-handler/Swipeable';
+import dayjs from 'dayjs';
+import {Button as DeleteButton} from 'src/components/UI/button/delete-button/delete-button';
+import {getPostsListsStyle} from '../posts-list/styles';
+
+type CustomFavourite = FavouritePosts['favouritePosts']['data'];
+type CustomPosts = Posts['posts']['data'];
+
+interface Props {
+  isError: boolean;
+  isLoading: boolean;
+  data: CustomFavourite | CustomPosts | undefined;
+  fetchMore: () => void;
+}
+
+const renderItemSwipe = (item: any) => {
+  const renderRightActions = () => {
+    return (
+      <View style={{width: 73}}>
+        <DeleteButton />
+      </View>
+    );
+  };
+
+  return (
+    <Swipeable renderRightActions={renderRightActions}>
+      <PostCard
+        postId={item.id}
+        key={item.id}
+        title={item.title}
+        createdAt={dayjs(item.createdAt).format('DD.MM.YY')}
+        description={item.description}
+        mediaUrl={item.mediaUrl}
+        avatarUrl={item.author.avatarUrl || ''}
+        authorName={formatAuthorName(
+          item.author.firstName || '',
+          item.author.lastName || '',
+        )}
+        likesCount={item.likesCount}
+        isLiked={item.isLiked}
+      />
+    </Swipeable>
+  );
+};
+
+export const SwipeablePostsList: React.FC<Props> = ({
+  isError,
+  isLoading,
+  data,
+  fetchMore,
+}) => {
+  const themeVariant = useColorTheme();
+  const styles = getPostsListsStyle(themeVariant);
+
+  return (
+    <>
+      <FlatList
+        contentContainerStyle={[styles.container, styles.containerBackground]}
+        data={data}
+        renderItem={({item}) => renderItemSwipe(item)}
+        keyExtractor={item => item.id}
+        onEndReached={fetchMore}
+        onEndReachedThreshold={0.1}
+        ListEmptyComponent={
+          <ListEmptyComponent isLoading={isLoading} isError={isError} />
+        }
+      />
+      {isLoading && data && (
+        <View style={styles.containerSpinner}>
+          <Spinner color={styles.spinnerColor} stroke={styles.spinnerStroke} />
+        </View>
+      )}
+    </>
+  );
+};
--- src/screens/posts/posts.tsx
+++ src/screens/posts/posts.tsx
@@ -1,10 +1,55 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, View, FlatList, Text} from 'react-native';
 import React from 'react';
+import {Button} from 'src/components/UI/button/button-icon-bg/button-icon-bg';
+import {SvgPlus} from 'src/shared/icons/components/plus-svg';
+import {PostsList} from 'src/components/posts-list/posts-list';
+import {usePostsRequest} from 'src/hooks/usePostsRequest';
+import {PostFilterType} from 'src/shared/types/__generated__/gql-types';
+import Swipeable from 'react-native-gesture-handler/Swipeable';
+import {Button as DeleteButton} from 'src/components/UI/button/delete-button/delete-button';
+import {PostCard} from 'src/components/UI/post-card/post-card';
+import {SwipeablePostsList} from 'src/components/swipeable-posts-list/swipeable-posts-list';
+
+const renderItemSwipe = (item: any) => {
+  const renderRightActions = () => {
+    return (
+      <View style={{width: 73}}>
+        <DeleteButton />
+      </View>
+    );
+  };
+
+  return (
+    <Swipeable renderRightActions={renderRightActions}>
+      <PostCard
+        postId={item.item.id}
+        key={item.item.id}
+        title={item.item.title}
+        createdAt={item.item.createdAt}
+        description={item.item.description}
+        mediaUrl={item.item.mediaUrl}
+        avatarUrl={item.item.author.avatarUrl || ''}
+        authorName={item.item.author.firstName || ''}
+        likesCount={item.item.likesCount}
+        isLiked={item.item.isLiked}
+      />
+    </Swipeable>
+  );
+};
 
 export const Posts = () => {
+  const [prevOpened, setPrevOpened] = React.useState<any>(null);
+  const {isLoading, isError, posts, fetchMore} = usePostsRequest({
+    type: PostFilterType.Top,
+  });
   return (
     <View style={styles.container}>
-      <Text>My posts</Text>
+      <SwipeablePostsList
+        isLoading={isLoading}
+        isError={isError}
+        data={posts}
+        fetchMore={fetchMore}
+      />
     </View>
   );
 };
@@ -12,7 +57,12 @@ export const Posts = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 8,
+  },
+  containerNewPost: {
+    width: '100%',
+    position: 'absolute',
+    bottom: 32,
+    left: '80%',
   },
 });
